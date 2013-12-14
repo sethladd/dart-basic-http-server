17ace2fb37a7b0eefcf2b9c7f848f142562d3f5b
Seth Ladd <sethladd@google.com>
2013-12-14 12:56:02 -0800

    first commit

---
+++ bin/basic_http_server.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'dart:async';
+import 'package:http_server/http_server.dart';
+
+void main() {
+  var staticFiles = new VirtualDirectory('.')
+    ..allowDirectoryListing = true;
+
+  runZoned(() {
+    HttpServer.bind('0.0.0.0', 7777).then((server) {
+      server.listen(staticFiles.serveRequest);
+    });
+  },
+  onError: (e, stackTrace) => print('Oh noes! $e $stackTrace'));
+}
